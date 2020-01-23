6d68acf7d4c53fbd3792ac0adcbd4932fc8e5317
SUVAGIYA AKASH <1akashsuvagiya1999@gmail.com>
2020-01-23 16:42:34 +0530

    Testing

--- app/src/main/java/com/everfino/everfinorest/MainActivity.java
+++ app/src/main/java/com/everfino/everfinorest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
     }
 }
